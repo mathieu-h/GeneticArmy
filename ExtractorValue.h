68ba9e3288b52085c382c93213a7a7b9ac0e211d
Mathieu Harharidis <hhds.mathieu@gmail.com>
2015-07-10 12:21:24 +0200

    More Extractors

--- ExtractorValue.h
+++ ExtractorValue.h
@@ -2,12 +2,12 @@
 #include "Extractor.h"
 
 
-class ExtractorV : Extractor<float>
+class ExtractorDirect : Extractor<float>
 {
 	float value;
 public:
-	ExtractorV(float value);
-	~ExtractorV();
+	ExtractorDirect(float value);
+	~ExtractorDirect();
 	float get(Unit currentUnit, Army ally, Army opp){
 		return value;
 	}
@@ -16,8 +16,10 @@ public:
 
 class ExtractorC : Extractor<float>
 {
+	int _index;
+	Extractor<Unit>* _eU;
 public:
-	ExtractorC();
+	ExtractorC(int index, Extractor<Unit>* eU) : _index(index), _eU(eU){};
 	~ExtractorC();
 	float get(Unit currentUnit, Army ally, Army opp);
 };
@@ -33,12 +35,74 @@ public:
 	float get(Unit currentUnit, Army ally, Army opp);
 };
 
-class ExtractorM: Extractor<float>
+
+class ExtractorM : Extractor<float>
 {
 private:
-	int index;
+	int _index;
+	Extractor<Army>* _eA;
 public:
-	ExtractorM(int index);
+	ExtractorM(int index, Extractor<Army>* eA) : _index(index){}, _eA(eA){};
 	~ExtractorM();
 	float get(Unit currentUnit, Army ally, Army opp);
 };
+
+class Extractorm : Extractor<float>
+{
+private:
+	int _index;
+	Extractor<Army>* _eA;
+public:
+	Extractorm(int index, Extractor<Army>* eA) : _index(index){}, _eA(eA){};
+	~Extractorm();
+	float get(Unit currentUnit, Army ally, Army opp);
+};
+
+class Extractora : Extractor<float>
+{
+private:
+	int _index;
+	Extractor<Army>* _eA;
+public:
+	Extractora(int index, Extractor<Army>* eA) : _index(index){}, _eA(eA){};
+	~Extractora();
+	float get(Unit currentUnit, Army ally, Army opp);
+};
+
+
+class ExtractorMD: Extractor<float>
+{
+private:
+	int _index;
+	Extractor<Army>* _eA;
+	Extractor<Point>* _eP;
+public:
+	ExtractorMD(int index, Extractor<Army>* eA, Extractor<Point>* eP) : _index(index){}, _eA(eA), _eP(eP){};
+	~ExtractorMD();
+	float get(Unit currentUnit, Army ally, Army opp);
+};
+
+class ExtractormD : Extractor<float>
+{
+private:
+	int _index;
+	Extractor<Army>* _eA;
+	Extractor<Point>* _eP;
+public:
+	ExtractormD(int index, Extractor<Army>* eA, Extractor<Point>* eP) : _index(index){}, _eA(eA), _eP(eP){};
+	~ExtractormD();
+	float get(Unit currentUnit, Army ally, Army opp);
+};
+
+class ExtractoraD : Extractor<float>
+{
+private:
+	int _index;
+	Extractor<Army>* _eA;
+	Extractor<Point>* _eP;
+public:
+	ExtractoraD(int index, Extractor<Army>* eA, Extractor<Point>* eP) : _index(index){}, _eA(eA), _eP(eP){};
+	~ExtractoraD();
+	float get(Unit currentUnit, Army ally, Army opp);
+};
+
