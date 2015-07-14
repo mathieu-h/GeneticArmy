0b59830b6be80647a4a6293147654cadd0506015
Mathieu Harharidis <hhds.mathieu@gmail.com>
2015-07-14 23:09:25 +0200

    Unit & Point completed

--- ExtractorPoint.h
+++ ExtractorPoint.h
@@ -8,7 +8,16 @@ private:
 public:
 	ExtractorB(u_ptr<Extractor<Army>> eA) : _eA(std::move(eA)){};
 	~ExtractorB();
-	Point get(Unit currentUnit, Army ally, Army opp);
+	// return the barycenter of all units in the set
+	Point get(Unit currentUnit, Army ally, Army opp){
+		std::vector<u_ptr<Unit>>& vUnit = _eA->get(currentUnit, ally, opp).getUnitsList();
+		float sumY = std::accumulate(vUnit.begin(), vUnit.end(),
+			0, [](float a, const Unit* it){return a + it->getPosition().getY(); });
+		float sumX = std::accumulate(vUnit.begin(), vUnit.end(),
+			0, [/*&sumY*/](float a, const Unit* it){return a + it->getPosition().getX(); 
+					/*sumY += it->getPosition().getY();*/ });
+		return vUnit.size() == 0 ? Point(.0f,.0f) : Point(sumX / vUnit.size(), sumY / vUnit.size());
+	}
 };
 
 class ExtractorP : Extractor<Point>
@@ -18,5 +27,7 @@ private:
 public:
 	ExtractorP(u_ptr<Extractor<Unit>> eU) : _eU(std::move(eU)){};
 	~ExtractorP();
-	Point get(Unit currentUnit, Army ally, Army opp);
+	Point get(Unit currentUnit, Army ally, Army opp){
+		return _eU->get(currentUnit, ally, opp).getPosition();
+	}
 };
--- ExtractorUnit.h
+++ ExtractorUnit.h
@@ -19,7 +19,9 @@ private:
 public:
 	ExtractorL(int index, u_ptr<Extractor<Army>> eA) : _index(index),_eA(std::move(eA)){};
 	~ExtractorL();
-	Unit get(Unit currentUnit, Army ally, Army opp);
+	Unit get(Unit currentUnit, Army ally, Army opp){
+		return _eA->get(currentUnit, ally, opp).getLowestUnit(_index);
+	}
 };
 
 class ExtractorH : Extractor<Unit>
@@ -30,7 +32,9 @@ private:
 public:
 	ExtractorH(int index, u_ptr<Extractor<Army>> eA) : _index(index), _eA(std::move(eA)){};
 	~ExtractorH();
-	Unit get(Unit currentUnit, Army ally, Army opp);
+	Unit get(Unit currentUnit, Army ally, Army opp){
+		return _eA->get(currentUnit, ally, opp).getHigestUnit(_index);
+	}
 };
 
 class ExtractorLD : Extractor<Unit>
@@ -43,7 +47,10 @@ public:
 	ExtractorLD(int index, u_ptr<Extractor<Army>> eA,
 		u_ptr<Extractor<Point>> eP) : _index(index), _eA(std::move(eA)), _eP(std::move(eP)){};
 	~ExtractorLD();
-	Unit get(Unit currentUnit, Army ally, Army opp);
+	Unit get(Unit currentUnit, Army ally, Army opp){
+		Point p = _eP->get(currentUnit, ally, opp);
+		return _eA->get(currentUnit, ally, opp).getNearestUnit(p);
+	}
 };
 
 class ExtractorHD : Extractor<Unit>
@@ -56,5 +63,8 @@ public:
 	ExtractorHD(int index, u_ptr<Extractor<Army>> eA,
 		u_ptr<Extractor<Point>> eP) : _index(index), _eA(std::move(eA)), _eP(std::move(eP)){};
 	~ExtractorHD();
-	Unit get(Unit currentUnit, Army ally, Army opp);
+	Unit get(Unit currentUnit, Army ally, Army opp){
+		Point p = _eP->get(currentUnit, ally, opp);
+		return _eA->get(currentUnit, ally, opp).getFurthestUnit(p);
+	}
 };
