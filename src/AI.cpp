37dc165034a0c58e4ac579b837167993f53eaaf5
Mathieu Harharidis <hhds.mathieu@gmail.com>
2015-07-18 10:58:20 +0200

    Préparation de la classe AI pour intégration du nouveau code

--- src/AI.cpp
+++ src/AI.cpp
@@ -14,42 +14,10 @@
 std::unique_ptr<Action> AI::operator()(Unit& unit, Army& allies, Army& opponents)
 {
     try {
-        if(unit.getFirerate().canShoot())
-        {
-            std::string code = unit.getIACode();
-            Unit* target;
+		// Intégrer la logique de la nouvelle IA
+		//
 
-            if(code[1] == 'D')
-            {
-                if(code[0]=='L')
-                    target = &(opponents.getNearestUnit(unit.getPosition()));
-                else
-                    target = &(opponents.getFurthestUnit(unit.getPosition()));
-            }
-            else
-            {
-                if(code[0]=='L')
-                    target = &(opponents.getLowestUnit(int(code[1]-'0')));
-                
-                else
-                    target = &(opponents.getHigestUnit(int(code[1]-'0')));
-            }
-            
-            if(target->getPosition().distance(unit.getPosition())>unit.getRange().getValue())
-            {
-                return std::unique_ptr<Action>(new MoveAction(unit, target->getPosition()));
-            }
-            else
-            {
-                return std::unique_ptr<Action>(new ShotAction(unit, *target));
-            }
-        }
-        else
-        {
-            Unit& target = opponents.getNearestUnit	(unit.getPosition());
-            Point p = (2*unit.getPosition()) - target.getPosition();
-            return std::unique_ptr<Action>(new MoveAction(unit, 1000*p));
-        }
+		return std::unique_ptr<Action>(new EmptyAction(unit));
     }
     catch(std::invalid_argument&)
     {
